f572faf79490241102fb32b4b06f43b627c4b364
DiegoCarlito <diego.carlito01@gmail.com>
2025-01-21 09:52:29 -0300

    feat: add solved exercises

---
+++ study/calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "Enter a number \n";
+    cin >> a;
+
+
+    cout << "Enter another number \n";
+    cin >> b;
+
+    sum = a + b;
+    cout << "Sum = " << sum << "\n";
+
+    return 0;
+}
---
+++ study/helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Standard Library
+// Set of data types and commands prepackaged with the language
+// namespace is something that can help you group things
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl; // Or just use \n to end line
+    return 0;
+}
