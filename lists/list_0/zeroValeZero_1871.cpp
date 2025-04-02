d02fed8a0a33c320ffdeab931ecad4c6a99da233
DiegoCarlito <diego.carlito01@gmail.com>
2025-04-01 23:59:44 -0300

    add solved exercise

---
+++ lists/list_0/zeroValeZero_1871.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    long int M, N;
+    cin >> M >> N;
+    while (M != 0 && N != 0)
+    {
+        long int sum = M + N;
+        string numString = to_string(sum);
+
+        numString.erase(remove(numString.begin(), numString.end(), '0'), numString.end());
+
+        cout << numString << endl;
+
+        cin >> M >> N;
+    }
+
+
+    return 0;
+}
